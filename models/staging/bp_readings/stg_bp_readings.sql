1c75919b9d3fe2211966a16e6ddbdacf14c4ec36
Rich Marafioti <richmarafioti@gmail.com>
2026-08-04 08:05:58 -0500

    fix: move sheet logic into stg for bp readings

--- models/staging/bp_readings/stg_bp_readings.sql
+++ models/staging/bp_readings/stg_bp_readings.sql
@@ -6,12 +6,18 @@ select
     time_of_day,
     systolic_reading,
     diastolic_reading,
-    -- transform for the full bp reading string in stg
-    bp_reading,
+    concat(systolic_reading, ' / ', diastolic_reading) as bp_reading,
     -- computed in the fact table
     systolic_change_from_previous_day,
     diastolic_change_from_previous_day,
-    -- transformed in staging
-    bp_category,
+    case 
+        when systolic_reading > 180 or diastolic_reading > 120 then 'Hypertensive Crisis'
+        when systolic_reading >= 140 or diastolic_reading >= 90 then 'Stage 2 Hypertension'
+        when systolic_reading >= 130 or diastolic_reading >= 80 and diastolic_reading < 90 then 'Stage 1 Hypertension'
+        when systolic_reading < 120 and diastolic_reading >= 80 and diastolic_reading < 90 then 'Isolated Diastolic Hypertension'
+        when systolic_reading >= 120 and systolic_reading < 130 and diastolic_reading < 80 then 'Elevated'
+        when systolic_reading < 120 and diastolic_reading < 80 then 'Normal'
+        else 'Check Reading'
+    end as bp_category,
     general_mood,
 from {{ source('bp_readings', 'bp_readings_raw') }}
