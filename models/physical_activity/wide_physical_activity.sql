0add82a7c4190d208dc8cb7b3dea92a3bf1a47f1
Rich Marafioti <richmarafioti@gmail.com>
2026-08-20 20:25:45 -0500

    feat: add weeks to wide physical activity

--- models/physical_activity/wide_physical_activity.sql
+++ models/physical_activity/wide_physical_activity.sql
@@ -14,6 +14,8 @@ select
 
     dim_date.month,
     dim_date.month_name,
+    dim_date.week_start_date,
+    dim_date.week_end_date,
     dim_date.year,
 
     dim_activity.time_of_day,
@@ -24,6 +26,14 @@ select
     fct_table.physical_activity_distance,
     fct_table.physical_activity_duration,
     fct_table.calories_burned,
+    -- cumulative hours of moderate and vigorous activity per week dimension
+    -- resets at the start of the new week
+    -- new dimension in an int model but first need a separate dimension for moderate and 
+    -- vigourous activity hours
+    -- window function checks if week start date and end date equal the previous records
+    -- then add to the dimension. if not reset to 0
+
+    -- flag for has met weekly moderate physical activity?
 
 from {{ ref('fct_physical_activity')}} as fct_table
 left join {{ ref('dim_physical_activity')}} as dim_activity
