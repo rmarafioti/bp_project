9e1675e5e4e5b0f0f78227659fb8a3c0319e3316
Rich Marafioti <richmarafioti@gmail.com>
2026-09-14 15:24:12 -0500

    bugfix: metric latest weekly activity goals - every person gets a row with a metric value and metric label whether they logged an activity in the current week or not

--- models/metrics/intermediate/latest_weekly_activity_goals.sql
+++ models/metrics/intermediate/latest_weekly_activity_goals.sql
@@ -1,100 +1,99 @@
-with latest_weekly_activity as (
+with current_week_dates as (
 
+    -- every date within the current week
+    select
+        date_key,
+        week_start_date
+    from {{ ref('dim_date') }}
+    where current_date("America/Chicago") between
+        week_start_date and week_end_date
+
+),
+
+current_week as (
+
+    -- the single current week start date
+    select distinct week_start_date
+    from current_week_dates
+
+),
+
+people as (
+
+    -- each person who has logged activity ever
+    select distinct person_key
+    from {{ ref('fct_physical_activity') }}
+
+),
+
+weekly_activity as (
+
+    -- all activity logged by a perosn for the current week
     select
         fct_table.person_key,
-        fct_table.physical_activity_intensity,
-        cast(dim_date.week_start_date as string) as week_start_date,
-
-        sum(fct_table.physical_activity_duration) as total_hours
-    from {{ ref('fct_physical_activity')}} as fct_table
-    left join {{ ref('dim_date') }} as dim_date
-        on fct_table.date_key = dim_date.date_key
-    group by 1,2,3
-    qualify row_number() over (
-        partition by
-            person_key,
-            physical_activity_intensity
-        order by week_start_date desc
-    ) = 1
+        sum(if(fct_table.physical_activity_intensity = 'Moderate Intensity',
+               fct_table.physical_activity_duration, 0)) as moderate_hours,
+        sum(if(fct_table.physical_activity_intensity in
+               ('Vigorous Intensity', 'Max Intensity'),
+               fct_table.physical_activity_duration, 0)) as vigorous_hours
+    from {{ ref('fct_physical_activity') }} as fct_table
+    inner join current_week_dates
+        on fct_table.date_key = current_week_dates.date_key
+    group by 1
 
 ),
 
-vigorous_intensity_hours as (
+results as (
 
+    -- ever person gets a row whether they logged acitivity or not
     select
-        person_key,
-        physical_activity_intensity,
-        week_start_date,
-        total_hours as vigorous_hours
-    from latest_weekly_activity
-    where physical_activity_intensity = 'Vigorous Intensity'
+        people.person_key,
+        cast(current_week.week_start_date as string)        as week_start_date,
+        coalesce(weekly_activity.moderate_hours, 0)         as moderate_hours,
+        coalesce(weekly_activity.vigorous_hours, 0)         as vigorous_hours
+    from people
+    cross join current_week
+    left join weekly_activity
+        on people.person_key = weekly_activity.person_key
 
-),
-
-max_intensity_hours as (
-
-    select
-        person_key,
-        physical_activity_intensity,
-        week_start_date,
-        total_hours as max_hours
-    from latest_weekly_activity
-    where physical_activity_intensity = 'Max Intensity'
-
-),
-
-full_vigorous_count as (
-
-    select
-        vigorous_intensity_hours.person_key,
-        vigorous_intensity_hours.week_start_date,
-        vigorous_intensity_hours.vigorous_hours + max_intensity_hours.max_hours as total_vigorous_hours,
-    from vigorous_intensity_hours
-    left join max_intensity_hours
-        on vigorous_intensity_hours.person_key = max_intensity_hours.person_key
-        and vigorous_intensity_hours.week_start_date = max_intensity_hours.week_start_date
-    
 ),
 
 metrics as (
-    
-    select
-        person_key,
-        'Physical Activity'                                     as metric_category,
-        'Activity Hours'                                        as metric_subcategory,
-        'Latest Weekly Moderate Activity Hours'                 as metric_name,
-        week_start_date                                         as metric_period,
-        cast(null as string)                                    as metric_reading,
-        total_hours                                             as metric_value,
-        case 
-            when total_hours > 2.5 then concat(total_hours - 2.5, ' Above Weekly Goal')
-            when total_hours = 2.5 then 'Met Weekly Goal'
-             when total_hours < 2.5 then concat(2.5 - total_hours, ' Below Weekly Goal')
-        end                                                     as metric_label,
-        if(total_hours >= 2.5, 1, 0)                            as has_met_goal
-    from latest_weekly_activity
-    where 
-        physical_activity_intensity = 'Moderate Intensity'
-
-union all 
 
     select
         person_key,
-        'Physical Activity'                                     as metric_category,
-        'Activity Hours'                                        as metric_subcategory,
-        'Latest Weekly Vigorous Activity Hours'                 as metric_name,
-        week_start_date                                         as metric_period,
-        cast(null as string)                                    as metric_reading,
-        total_vigorous_hours                                    as metric_value,
+        'Physical Activity'                         as metric_category,
+        'Activity Hours'                            as metric_subcategory,
+        'Latest Weekly Moderate Activity Hours'     as metric_name,
+        week_start_date                             as metric_period,
+        cast(null as string)                        as metric_reading,
+        moderate_hours                              as metric_value,
         case
-            when total_vigorous_hours > 1.25 then concat(total_vigorous_hours - 1.25, ' Above Weekly Goal')
-            when total_vigorous_hours = 1.25 then 'Met Weekly Goal'
-            when total_vigorous_hours < 1.25 then concat(1.25 - total_vigorous_hours, ' Below Weekly Goal')
-        end                                                     as metric_label,
-        if(total_vigorous_hours >= 1.25, 1, 0)                  as has_met_goal
-    from full_vigorous_count
+            when moderate_hours > 2.5 then concat(moderate_hours - 2.5, ' Above Weekly Goal')
+            when moderate_hours = 2.5 then 'Met Weekly Goal'
+            when moderate_hours < 2.5 then concat(2.5 - moderate_hours, ' Below Weekly Goal')
+        end                                         as metric_label,
+        if(moderate_hours >= 2.5, 1, 0)             as has_met_goal
+    from results
+
+    union all
+
+    select
+        person_key,
+        'Physical Activity'                         as metric_category,
+        'Activity Hours'                            as metric_subcategory,
+        'Latest Weekly Vigorous Activity Hours'     as metric_name,
+        week_start_date                             as metric_period,
+        cast(null as string)                        as metric_reading,
+        vigorous_hours                              as metric_value,
+        case
+            when vigorous_hours > 1.25 then concat(vigorous_hours - 1.25, ' Above Weekly Goal')
+            when vigorous_hours = 1.25 then 'Met Weekly Goal'
+            when vigorous_hours < 1.25 then concat(1.25 - vigorous_hours, ' Below Weekly Goal')
+        end                                         as metric_label,
+        if(vigorous_hours >= 1.25, 1, 0)            as has_met_goal
+    from results
 
 )
 
 select * from metrics
-
